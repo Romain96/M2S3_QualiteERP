c37f305e70efa07d600ba9ec91d48bdf374ee8f6
Romain PERRIN <romain.perrin@etu.unistra.fr>
2018-10-22 23:06:38 +0200

    checking new date (sort of)

--- src/date_dialog.cpp
+++ src/date_dialog.cpp
@@ -24,6 +24,31 @@ void DateDialog::on_buttonBox_accepted()
     int month = ui->lineEdit_month->text().toInt();
     int day = ui->lineEdit_day->text().toInt();
 
+    // checling values
+    if (month < 1)
+        month = 1;
+    else if (month > 12)
+        month = 12;
+
+    if (day < 1)
+        day = 0;
+    else if (day > 31)
+        day = 31;
+
+    if (month == 2 && day > 28)
+    {
+        if (day > 28)
+            day = 28;
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        if (day > 30)
+            day = 30;
+    }
+
+    if (year < 1)
+        year = 1;
+
     emit starting_date_send_new_date(year, month, day);
 
     close();
